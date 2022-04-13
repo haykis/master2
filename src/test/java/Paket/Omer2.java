eb2e901170da81a1ab0253e3f9bd76252375bdf7
OMER <omertufan26@yandex.com>
2022-04-13 17:40:34 +0100

    iki

--- src/test/java/Paket/Omer2.java
+++ src/test/java/Paket/Omer2.java
@@ -5,5 +5,6 @@ public class Omer2 {
     public static void main(String[] args) {
 
         System.out.println("Rahman ve rahim olan Allah' in adiyla...");
+        System.out.println("pull islemini yapabilirsin");
     }
 }
