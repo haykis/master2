db59947ac56563549c2b791d59c965782508d484
Huseyin Aykis <h.aykis25@gmail.com>
2022-04-13 18:56:33 +0200

    ol

--- src/test/java/Paket/Huseyin2.java
+++ src/test/java/Paket/Huseyin2.java
@@ -2,6 +2,9 @@ package Paket;
 
 public class Huseyin2 {
     public static void main(String[] args) {
+
         System.out.println("yeniden basliyoruz");
+
+      Omer2.deneme(145000.6f,"tl degerimi euro ya hesaplamaya basliyorum");
     }
 }
