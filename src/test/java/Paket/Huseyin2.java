2be104bc01a86eb9ff7f11d196b5cf688b6a4130
Huseyin Aykis <h.aykis25@gmail.com>
2022-04-13 18:27:54 +0200

    merhaba

---
+++ src/test/java/Paket/Huseyin2.java
@@ -0,0 +1,7 @@
+package Paket;
+
+public class Huseyin2 {
+    public static void main(String[] args) {
+        System.out.println("yeniden basliyoruz");
+    }
+}
---
+++ src/test/java/Paket/Omer2.java
@@ -0,0 +1,4 @@
+package Paket;
+
+public class Omer2 {
+}
